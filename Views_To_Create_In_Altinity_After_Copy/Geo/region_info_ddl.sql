d55ed3ca9285229c882d1b610d868970e264195a
Mithun <137824587+mit311@users.noreply.github.com>
2023-12-01 10:54:13 -0500

    Update region_info_ddl.sql

--- Views_To_Create_In_Altinity_After_Copy/Geo/region_info_ddl.sql
+++ Views_To_Create_In_Altinity_After_Copy/Geo/region_info_ddl.sql
@@ -1,15 +1,15 @@
-CREATE VIEW info.region_info
+CREATE VIEW info_test.region_info ON CLUSTER 'reporting-dev01'
 (
- `region` String,
- `country` Nullable(String),
- `iso_code` Nullable(String),
- `country_iso_code` Nullable(String)
+ region String,
+ country Nullable(String),
+ iso_code Nullable(String),
+ country_iso_code Nullable(String)
 ) AS
 SELECT
  r.location AS region,
  r.sub_location1 AS country,
  r.iso_code,
  c.iso_code AS country_iso_code
-FROM info.location_data AS r
-INNER JOIN info.location_data AS c ON r.sub_location1 = c.location
-WHERE (r.location_type_id = 5) AND (c.location_type_id = 2)
+FROM info_test.location_data AS r
+INNER JOIN info_test.location_data AS c ON r.sub_location1 = c.location
+WHERE (r.location_type_id = 5) AND (c.location_type_id = 2);
