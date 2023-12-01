954d1920cf244183250363e4bb7cf9f11fe2f480
Mithun <137824587+mit311@users.noreply.github.com>
2023-12-01 10:53:38 -0500

    Update city_ddl.sql

--- Views_To_Create_In_Altinity_After_Copy/Geo/city_ddl.sql
+++ Views_To_Create_In_Altinity_After_Copy/Geo/city_ddl.sql
@@ -1,11 +1,11 @@
-CREATE VIEW info.city
+CREATE VIEW info_test.city ON CLUSTER 'reporting-dev01'
 (
- `city` String,
- `region` String,
- `country` Nullable(String),
- `parent_iso_code` Nullable(String),
- `name` Nullable(String)
-) AS
+ city String,
+ region String,
+ country Nullable(String),
+ parent_iso_code Nullable(String),
+ name Nullable(String)
+)  AS
 WITH CTE AS
  (
  SELECT
@@ -13,8 +13,8 @@ WITH CTE AS
  state.location AS region,
  state.sub_location1 AS country,
  state.iso_code AS parent_iso_code
- FROM info.location_data AS city
- INNER JOIN info.location_data AS state ON city.sub_location1 = state.location
+ FROM info_test.location_data AS city
+ INNER JOIN info_test.location_data AS state ON city.sub_location1 = state.location
  WHERE (city.location_type_id = 6) AND (state.location_type_id = 5)
  )
 SELECT
@@ -22,5 +22,5 @@ SELECT
  region,
  country,
  parent_iso_code,
- multiIf(city = \'-1\', \'unknown\', parent_iso_code IS NOT NULL, concat(city, \' \', parent_iso_code), city) AS name
-FROM CTE
+ multiIf(city = '-1', 'unknown', parent_iso_code IS NOT NULL, concat(city, ' ', parent_iso_code), city) AS name
+FROM CTE;
