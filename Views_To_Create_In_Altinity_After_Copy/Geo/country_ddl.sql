1e337b2323da065deaebdca4026714148b0838eb
Mithun <137824587+mit311@users.noreply.github.com>
2023-12-01 10:54:31 -0500

    Update country_ddl.sql

--- Views_To_Create_In_Altinity_After_Copy/Geo/country_ddl.sql
+++ Views_To_Create_In_Altinity_After_Copy/Geo/country_ddl.sql
@@ -1,4 +1,4 @@
-CREATE VIEW info.country
+CREATE VIEW info_test.country ON CLUSTER 'reporting-dev01'
 (
  `country` String,
  `iso_code` String
@@ -6,5 +6,5 @@ CREATE VIEW info.country
 SELECT
  c.location AS country,
  c.iso_code AS iso_code
-FROM info.location_data AS c
-WHERE c.location_type_id = 2
+FROM info_test.location_data AS c
+WHERE c.location_type_id = 2;
